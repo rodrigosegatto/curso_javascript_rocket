79edadda2cb45f03dd6a181d2532d5630c0f459f
rodrigosegatto <Segattorodrigo@hotmail.com>
2020-03-16 23:10:51 -0300

    Exercício Final 1 finalizado

--- 5-Desafio_final/exercicio1/main.js
+++ 5-Desafio_final/exercicio1/main.js
@@ -5,3 +5,23 @@ retornará se usuário é maior ou não que 18 anos.
 Se o usuário ter mais que 18 anos de idade o 
 resultado deve cair no .then, caso contrário, no .catch
 */
+
+var checkIdade = function (idade){
+    return new Promise(function(resolve,reject){
+        setInterval(function(){
+            if(idade>18){
+                resolve(true);
+            }else{
+                reject(false);
+            }
+        },2000);
+    });
+}
+
+checkIdade(20)
+    .then(function(resolve){
+        console.log("Maior que 18 anos!");
+    })
+    .catch(function(reject){
+        console.warn("Menor que 18 anos!");
+    });
